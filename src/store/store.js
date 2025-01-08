2be2c8cdd0c78919e0a0aad6891a43df2eeda455
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-08 21:37:33 +0530

    add trendingCoinApi reducer and middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
+import { trendingCoinApi } from "../services/trendingCoinApi";
 
 const store = configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [trendingCoinApi.reducerPath]: trendingCoinApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cryptoApi.middleware),
+    getDefaultMiddleware().concat(
+      cryptoApi.middleware,
+      trendingCoinApi.middleware
+    ),
 });
 
 export default store;
