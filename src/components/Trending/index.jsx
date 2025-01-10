09e4b3bb6935cb284a69c2ddecbe244d6ca9a21c
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-10 13:22:15 +0530

    refactor trending component for improved image handling and layout

--- src/components/Trending/index.jsx
+++ src/components/Trending/index.jsx
@@ -1,16 +1,15 @@
 const Card = ({ id, name, price, image, marketCap, score }) => {
+  console.log(image)
   return (
-    <div className="bg-[#303030] flex items-center justify-between w-[500px] h-[140px] rounded-md px-4 ">
-      <div>
+    <div className="bg-[#303030] flex items-center gap-8  lg:w-[500px] h-[140px] rounded-md px-4 ">
+      <div className="w-[60px] ">
         <img
           src={
             image ||
             "https://coin-images.coingecko.com/coins/images/53183/large/1000281355.jpg?1735563013"
           }
           alt={name}
-          width={100}
-          height={100}
-          className="rounded-full"
+          className="rounded-full w-full h-full"
         />
       </div>
       <div className="flex flex-col gap-1">
