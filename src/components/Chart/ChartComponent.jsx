dc5fd039131f8953d48a8a1df1a0140ab3564c7e
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-10 13:06:37 +0530

    add ChartComponent to display cryptocurrency historical data

---
+++ src/components/Chart/ChartComponent.jsx
@@ -0,0 +1,102 @@
+import { useCallback, useEffect, useState } from "react";
+import { useLazyGetCoinHistoryDataQuery } from "../../services/cryptoApi";
+import Chart from "./Chart";
+
+const ChartComponent = ({ id }) => {
+  const [type, setType] = useState("prices");
+  const [days, setDays] = useState(7);
+  const [chartData, setChartData] = useState([]);
+  const [cryptoData, setCryptoData] = useState([]);
+
+  const [trigger, { data, isFetching }] = useLazyGetCoinHistoryDataQuery();
+
+  const fetchCoinHistory = useCallback(() => {
+    if (days && id) {
+      trigger({ coinId: id, vs_currency: "usd", days });
+    }
+  }, [id, days, trigger]);
+
+  useEffect(() => {
+    fetchCoinHistory();
+  }, [fetchCoinHistory]);
+
+  useEffect(() => {
+    if (data) {
+      setCryptoData(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (cryptoData && type) {
+      const convertedData = cryptoData[type]?.map((item) => ({
+        date: new Date(item[0]).toLocaleDateString(),
+        [type]: item[1],
+      }));
+      setChartData(convertedData || []);
+    }
+  }, [type, cryptoData]);
+
+  if (isFetching) return "loading...";
+
+  return (
+    <div>
+      <div>
+        <div className="bg-[#2f2f2f] mb-4  flex justify-cente   gap-1 px-1  py-2 rounded-md">
+          <button
+            onClick={() => setType("prices")}
+            className={`${
+              type === "prices" ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            Price
+          </button>
+          <button
+            onClick={() => setType("market_caps")}
+            className={`${
+              type === "market_caps" ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            Market Cap
+          </button>
+          <button
+            onClick={() => setType("total_volumes")}
+            className={`${
+              type === "total_volumes" ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            Total Volume
+          </button>
+          <button
+            onClick={() => setDays(7)}
+            className={`${
+              days === 7 ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            7D
+          </button>
+          <button
+            onClick={() => setDays(14)}
+            className={`${
+              days === 14 ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            14D
+          </button>
+          <button
+            onClick={() => setDays(30)}
+            className={`${
+              days === 30 ? "bg-[#FFB9B3] text-black " : ""
+            } font-semibold lg:font-bold px-2 rounded text-sm`}
+          >
+            30D
+          </button>
+        </div>
+        <div className="lg:w-[500px]">
+          <Chart data={chartData} type={type} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartComponent;
