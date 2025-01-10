541697ca55ed18ad2ea258d518488ea2dd6d0e2b
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-10 13:21:43 +0530

    add HighLowIndicator component

---
+++ src/components/Indicator/HighLowIndicator.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const HighLowIndicator = ({ currentPrice, high, low }) => {
+  const [colorValue, setColorValue] = useState(null);
+
+  useEffect(() => {
+    let total = high - low;
+
+    let colorPercentage = ((high - currentPrice) * 100) / total;
+
+    setColorValue(Math.ceil(colorPercentage));
+  }, [currentPrice, high, low]);
+
+  return (
+    <div className="flex px-2">
+      <span
+        className="bg-red-600 h-1.5 rounded-l-lg w-[50%]"
+        style={{ width: `${100 - colorValue}%` }}
+      ></span>
+      <span
+        className="bg-green-600 h-1.5 rounded-r-lg w-[50%]"
+        style={{ width: `${colorValue}%` }}
+      ></span>
+    </div>
+  );
+};
+
+export default HighLowIndicator;
