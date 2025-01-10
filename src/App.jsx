21613befb2f11ef7691efcf30c94c47ae52d44b5
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-10 12:59:02 +0530

    add routes for cryptoDetails and trendingCoin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./Pages/Home/Home";
+import CryptoDetails from "./Pages/Crypto Details/CryptoDetails";
+import TrendingCoins from "./Pages/Trending/TrendingCoins";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<CryptoDetails />} />
+          <Route path="/trending" element={<TrendingCoins />} />
         </Route>
       </Routes>
     </BrowserRouter>
