a9f60ea39a22cfcc9110746310056dea9f993a27
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2025-01-10 13:00:19 +0530

    refactor home component to include link for coin details and optimize table data mapping

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import { useGetCoinListQuery } from "../../services/cryptoApi";
 import millify from "millify";
 import Table from "../../components/table/Table";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { data: coinList, isLoading } = useGetCoinListQuery("usd");
+
+  const tableData =
+    coinList?.map((coinData, index) => ({
+      index: index + 1,
+      coin: {
+        id: `${coinData.id}`,
+        icon: `${coinData.image}`,
+        name: `${coinData.name}`,
+        symbol: `${coinData.symbol}`,
+      },
+      price: `$${coinData.current_price}`,
+      market_cap: millify(coinData.market_cap),
+      total_volume: millify(coinData.total_volume),
+    })) || [];
 
   const columns = [
     { accessorKey: "index", header: "#" },
@@ -13,11 +28,11 @@ const Home = () => {
       cell: (row) => {
         const coin = row.getValue();
         return (
-          <div className="flex gap-3 items-center">
+          <Link to={`/${coin.id}`} className="flex gap-3 items-center">
             <img src={coin.icon} alt="" width={25} height={25} />
-            <p className="font-semibold text-xl">{coin.name}</p>
+            <p className="font-semibold lg:text-xl">{coin.name}</p>
             <p>{coin.symbol}</p>
-          </div>
+          </Link>
         );
       },
     },
@@ -25,19 +40,6 @@ const Home = () => {
     { accessorKey: "price", header: "Price" },
     { accessorKey: "market_cap", header: "Market Cap Changes" },
   ];
-
-  const tableData =
-    coinList?.map((coinData, index) => ({
-      index: index + 1,
-      coin: {
-        icon: `${coinData.image}`,
-        name: `${coinData.name}`,
-        symbol: `${coinData.symbol}`,
-      },
-      price: `$${coinData.current_price}`,
-      market_cap: millify(coinData.market_cap),
-      total_volume: millify(coinData.total_volume),
-    })) || [];
 
   if (isLoading) return "loading...";
 
