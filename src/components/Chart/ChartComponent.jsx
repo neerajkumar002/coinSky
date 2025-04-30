5ab611f26482469859cd7c7b9d962ef9bc01990f
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-30 23:22:36 +0530

    refactor: ChartComponent for improved loading state and layout

--- src/components/Chart/ChartComponent.jsx
+++ src/components/Chart/ChartComponent.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { useLazyGetCoinHistoryDataQuery } from "../../services/cryptoApi";
-import Chart from "./Chart";
+import Chart from "./Chart"; 
 
 const ChartComponent = ({ id }) => {
   const [type, setType] = useState("prices");
@@ -36,66 +36,69 @@ const ChartComponent = ({ id }) => {
     }
   }, [type, cryptoData]);
 
-  if (isFetching) return "loading...";
+  if (isFetching)
+    return (
+      <div className=" w-full h-[600px]  flex justify-center items-center  ">
+        <div className="w-12 h-12 border-4 border-t-transparent border-t-red-400 rounded-full animate-spin"></div>
+      </div>
+    );
 
   return (
- 
-      <div className="py-6">
-        <div className="bg-[#2f2f2f] mb-4  flex justify-between   gap-1 px-1  py-2 rounded-md lg:w-[500px]">
-          <button
-            onClick={() => setType("prices")}
-            className={`${
-              type === "prices" ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            Price
-          </button>
-          <button
-            onClick={() => setType("market_caps")}
-            className={`${
-              type === "market_caps" ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            Market Cap
-          </button>
-          <button
-            onClick={() => setType("total_volumes")}
-            className={`${
-              type === "total_volumes" ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            Total Volume
-          </button>
-          <button
-            onClick={() => setDays(7)}
-            className={`${
-              days === 7 ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            7D
-          </button>
-          <button
-            onClick={() => setDays(14)}
-            className={`${
-              days === 14 ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            14D
-          </button>
-          <button
-            onClick={() => setDays(30)}
-            className={`${
-              days === 30 ? "bg-[#FFB9B3] text-black " : ""
-            } font-semibold lg:font-bold px-2 rounded text-sm`}
-          >
-            30D
-          </button>
-        </div>
-        <div className="lg:w-full">
-          <Chart data={chartData} type={type} />
-        </div>
+    <div className=" px-3">
+      <div className="bg-[#2f2f2f] mb-4  flex justify-between   gap-1 px-1  py-2 rounded-md lg:w-[500px]">
+        <button
+          onClick={() => setType("prices")}
+          className={`${
+            type === "prices" ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          Price
+        </button>
+        <button
+          onClick={() => setType("market_caps")}
+          className={`${
+            type === "market_caps" ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          Market Cap
+        </button>
+        <button
+          onClick={() => setType("total_volumes")}
+          className={`${
+            type === "total_volumes" ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          Total Volume
+        </button>
+        <button
+          onClick={() => setDays(7)}
+          className={`${
+            days === 7 ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          7D
+        </button>
+        <button
+          onClick={() => setDays(14)}
+          className={`${
+            days === 14 ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          14D
+        </button>
+        <button
+          onClick={() => setDays(30)}
+          className={`${
+            days === 30 ? "bg-[#FFB9B3] text-black " : ""
+          } font-semibold lg:font-bold px-2 rounded text-sm`}
+        >
+          30D
+        </button>
       </div>
- 
+      <div className="lg:w-full">
+        <Chart data={chartData} type={type} />
+      </div>
+    </div>
   );
 };
 
